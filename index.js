6ca047060bf6a63a7d275d9b7e6b6ff0fc3527f6
Jason Hughes <jasonhughes088@gmail.com>
2018-02-16 18:19:30 -0700

    Extensions

--- index.js
+++ index.js
@@ -7,9 +7,25 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+io.on('connection', socket => {
+  const date = Date.now()
+  const lastFour = date.toString().split('').splice(9,4).join('')
+  let user = `ID: ${lastFour}`
+
+  console.log('A user has connected.');
+  socket.broadcast.emit('user connect', `User ${user} has signed on!`);
+
+  socket.on('typing', (msg) => {
+    socket.broadcast.emit('other user typing', msg)
+  })
+
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', `${user}: ${msg}`);
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('user disconnect', `User ${user} has signed off :(`)
+    console.log('User disconnected.');
   });
 });
 
